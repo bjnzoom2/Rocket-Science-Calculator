c94102f80070635fcf67a34bdf229be62e4cecb9
bjnzoom2 <bjnzoom2@gmail.com>
2026-04-24 20:43:40 +0800

    Minor Change

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ int main() {
 	float dryMass = 0.015f + 0.0056 + 0.005;
 	Rocket waterRocket(mfr, exhaustVelo, seaLevelPa, seaLevelPa, exitArea, dryMass, 0.3f);
 
-	std::cout << "Net Force: " << waterRocket.getNetForce() << "N\n";
-	std::cout << "Acceleration: " << waterRocket.getAccel() << "m/s2\n";
-	std::cout << "Burn Time: " << waterRocket.getBurnTime() << "s\n";
+	std::cout << "Net Force: " << waterRocket.getNetForce() << " N\n";
+	std::cout << "Acceleration: " << waterRocket.getAccel() << " m/s2\n";
+	std::cout << "Burn Time: " << waterRocket.getBurnTime() << " s\n";
 
 	return 0;
 }
